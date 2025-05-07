947bd8a2ba6e0c13b8ff8237617bf8e176b0b76c
Mohammad Fayzi <fayzi.dev@gmail.com>
2025-05-07 17:12:00 +0330

    Face Landmarks by Mediapipe on OpenCV
    Face Landmarks by Mediapipe on OpenCV

---
+++ Face_Landmarks.py
@@ -0,0 +1,41 @@
+import cv2
+import mediapipe as mp
+
+# Initialize Mediapipe Face Mesh
+mp_face_mesh = mp.solutions.face_mesh
+mp_drawing = mp.solutions.drawing_utils
+
+# Start processing the live video
+cap = cv2.VideoCapture("C:/Users/fayzi-dev/Desktop/ComputerVision/my_video.mp4")
+with mp_face_mesh.FaceMesh(
+    max_num_faces=1,  # Number of faces 
+    refine_landmarks=True,  # For more precise landmarks on eyes and lips 
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5) as face_mesh:
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Convert BGR to RGB for Mediapipe
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = face_mesh.process(frame_rgb)
+
+        # Draw points
+        if results.multi_face_landmarks:
+            for face_landmarks in results.multi_face_landmarks:
+                mp_drawing.draw_landmarks(
+                    frame,
+                    face_landmarks,
+                    mp_face_mesh.FACEMESH_TESSELATION,  # To draw the complete face mesh
+                    landmark_drawing_spec=None,
+                    connection_drawing_spec=mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=1)
+                )
+
+        cv2.imshow('Face Mesh', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
